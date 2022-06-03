c9887dd709c07d964b9b1897cfcb05b70d41b314
Tripop Torcheep <tripop.tor@qualint.io>
2022-06-04 01:42:13 +0700

    SECTION4 signup

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { User } from './user.entity';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+    const user = this.userRepository.create({
+      username,
+      password,
+    });
+    await this.userRepository.save(user);
+    return;
+  }
+}
