4e637054cec648c90c19dac261f670b5326a4b4c
Tripop Torcheep <tripop.tor@qualint.io>
2022-06-04 14:04:29 +0700

    SECTION5 error handling username conflict

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
@@ -16,7 +20,14 @@ export class AuthService {
       username,
       password,
     });
-    await this.userRepository.save(user);
-    return;
+    try {
+      await this.userRepository.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
